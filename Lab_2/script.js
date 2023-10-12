6e551aa9750adaea95389612caa091aeafd020b4
Adrian <adrianwzorek@interia.pl>
2023-10-12 12:36:41 +0200

    Zmiana

--- Lab_2/script.js
+++ Lab_2/script.js
@@ -6,7 +6,9 @@ function showTime() {
 
   let hour = date.getHours();
   let min = date.getMinutes();
+  min = min < 10 ? "0" + min : min;
   let sec = date.getSeconds();
+  sec = sec < 10 ? "0" + sec : sec;
 
   document.getElementById("date").innerHTML = day + "/" + mounth + "/" + year;
   document.getElementById("time").innerHTML = hour + ":" + min + ":" + sec;
