a24cf14c19e4477b78b7b348dc5301326884a47e
Adrian <adrianwzorek@interia.pl>
2023-10-19 12:42:08 +0200

    zmiana

---
+++ Lab_1/js/house.js
@@ -0,0 +1,23 @@
+$("#name").hide();
+$("#people").hide();
+$("#info").hide();
+$("#hero").hide();
+$("th").hide();
+const showTitle = () => {
+  $("#people").fadeToggle(3000);
+  $("#name").fadeIn(2000);
+  $("#info").fadeIn(2000);
+  $("#hero").fadeToggle(2000);
+  $("#herb").animate(
+    {
+      left: 20,
+      top: "8%",
+      fontSize: "120%",
+    },
+    2000
+  );
+};
+
+const showStudent = () => {
+  $("th").fadeToggle(1000);
+};
